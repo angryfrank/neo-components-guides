c104bddddf2446f5d6a9e9f740c2db9afb218f25
guanlong.xu <guanlong.xu@xinguang.com>
2018-09-12 16:18:44 +0800

    标签多选框说明修改
    Signed-off-by: guanlong.xu <guanlong.xu@xinguang.com>

--- src/views/checkboxtaggroup-comp.vue
+++ src/views/checkboxtaggroup-comp.vue
@@ -3,37 +3,45 @@
 		<fieldset>
 			<legend>控件展示</legend>
 			<neo-checkbox-tag-group v-model="cbList">
-				<neo-checkbox-tag v-for="item in itemList" :key="item.key" :item="item"></neo-checkbox-tag>
-				<neo-checkbox-tag :item="item" disabled></neo-checkbox-tag>
+				<neo-checkbox-tag v-for="item in itemList" :key="item.label" :label="item.label" @click="handleClick(item.label, item.value,$event)">{{item.value}}</neo-checkbox-tag>
+				<neo-checkbox-tag :label="item.label" disabled>{{item.value}}</neo-checkbox-tag>
 			</neo-checkbox-tag-group>
 		</fieldset>
 		<fieldset>
 			<legend>控件代码</legend>
 			<textarea readonly>
-					<neo-checkbox-tag-group v-model="cbList">
-						<neo-checkbox-tag v-for="item in itemList" :key="item.key" :item="item"></neo-checkbox-tag>
-						<neo-checkbox-tag :item="item" disabled></neo-checkbox-tag>
-					</neo-checkbox-tag-group>
-				</textarea>
+//中括号请使用花括号替换
+<neo-checkbox-tag-group v-model="cbList">
+	<neo-checkbox-tag v-for="item in itemList" :key="item.label" :label="item.label" @click="handleClick(item.label, item.value,$event)">[[item.value]]</neo-checkbox-tag>
+	<neo-checkbox-tag :label="item.label" disabled>[[item.value]]</neo-checkbox-tag>
+</neo-checkbox-tag-group>
+			</textarea>
 		</fieldset>
 		<fieldset>
 			<legend>属性说明</legend>
 			<textarea readonly>
-				cbList:[1,2],
-				itemList: [{
-					key:1,
-					value: "大大促销",
-				}, {
-					key:2,
-					value: "小促销",
-				}, {
-					key:3,
-					value: "大中促销",
-				}],
-				item: {
-					key:4,
-					value: "大大促销",
-				},
+//label值支持String
+cbList: ["1", "2"],
+itemList: [{
+	label: "1",
+	value: "大大促销",
+}, {
+	label: "2",
+	value: "小促销",
+}, {
+	label: "3",
+	value: "大中促销",
+}],
+item: {
+	label: "4",
+	value: "大大促销",
+},
+
+function handleClick(label, value, evt) {
+	console.log("label", label);
+	console.log("value", value);
+	console.log("evt", evt);//返回的选中状态,true/false
+}
 			</textarea>
 		</fieldset>
 	</div>
@@ -44,24 +52,31 @@
 		name: 'CheckboxGroupComp',
 		data() {
 			return {
-				cbList:[1,2],
+				cbList: ["1", "2"],
 				itemList: [{
-					key:1,
+					label: "1",
 					value: "大大促销",
 				}, {
-					key:2,
+					label: "2",
 					value: "小促销",
 				}, {
-					key:3,
+					label: "3",
 					value: "大中促销",
 				}],
 				item: {
-					key:4,
+					label: "4",
 					value: "大大促销",
 				},
 			}
 		},
-		methods: {},
-		watch:{}
+		methods: {
+			handleClick
+		},
+	}
+
+	function handleClick(label, value, evt) {
+		console.log("label", label);
+		console.log("value", value);
+		console.log("evt", evt);
 	}
 </script>
